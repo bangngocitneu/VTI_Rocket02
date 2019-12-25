2efab4dca868c137bd79474c6affd23c4bcb1afa
bangngoitneu <ngocmapb1@gmail.com>
2019-12-25 17:03:51 +0700

    sua assignment 4

--- mySQL/assignment4/Assignment4.sql
+++ mySQL/assignment4/Assignment4.sql
@@ -50,25 +50,25 @@ CREATE TABLE 				Employee_Skill_Table
 (
 	EmployeeNumber		TINYINT,
     SkillCode			VARCHAR(10),
-	DateRegistered		VARCHAR(20),
+	DateRegistered		DATE,
     FOREIGN KEY (EmployeeNumber) REFERENCES Employee_Table(EmployeeNumber)
 );
 INSERT INTO 	Employee_Skill_Table (EmployeeNumber,	SkillCode,	DateRegistered)
-VALUES								 (		5		,	"SK04"	 ,	".NET"),
-									 (		1		,	"SK01"	 ,	"JAVA"),
-									 (		2		,	"SK02"	 ,	"PHP"),
-                                     (		3		,	"SK03"	 ,	"PYTHON"),
-                                     (		3		,	"SK01"	 ,	"PHP"),
-									 (		5		,	"SK02"	 ,	"JAVA"),
-                                     (		1		,	"SK01"	 ,	"PYTHON"),
-									 (		2		,	"SK02"	 ,	"C"),
-                                     (		4		,	"SK04"	 ,	"C");
+VALUES								 (		5		,	"JAVA"	 ,	'2005-01-20'),
+									 (		1		,	"PHP"	 ,	'2006-02-21'),
+									 (		2		,	"C"	 	 ,	'2004-01-21'),
+                                     (		3		,	"JAVA"	 ,	'2005-08-20'),
+                                     (		3		,	"PHP"	 ,	'2004-10-18'),
+									 (		5		,	"C"	 	 ,	'2003-12-22'),
+                                     (		1		,	"C"	 	 ,	'2005-03-25'),
+									 (		2		,	"PHP"	 ,	'2005-01-26'),
+                                     (		4		,	"PHP"	 ,	'2005-11-28');
                                     
 	-- CAU 3
-SELECT		E.EmployeeNumber,EmployeeName, DateRegistered AS Skill
+SELECT		E.EmployeeNumber,EmployeeName, SkillCode AS Skill
 FROM		Employee_Table AS E 
 JOIN 		Employee_Skill_Table AS S 	ON		E.EmployeeNumber = S.EmployeeNumber 
-WHERE 		DateRegistered = "JAVA";
+WHERE 		SkillCode = "JAVA";
 -- CAU 4
 SELECT 		D.DepartmentNumber,DepartmentName
 FROM		Department AS D
@@ -81,10 +81,10 @@ FROM		Department AS D
 JOIN		Employee_Table AS E	ON D.DepartmentNumber = E.DepartmentNumber
 GROUP BY(D.DepartmentNumber);
 -- CAU 6
-SELECT		E.EmployeeNumber,EmployeeName,GROUP_CONCAT(DateRegistered) AS Skill 
+SELECT		E.EmployeeNumber,EmployeeName,GROUP_CONCAT(SkillCode) AS Skill 
 FROM		Employee_Table  AS E
 JOIN		Employee_Skill_Table AS S ON E.EmployeeNumber = S.EmployeeNumber
-GROUP BY(S.SkillCode)
+GROUP BY(S.EmployeeNumber)
 HAVING		COUNT(S.SkillCode) > 1;
 
 
