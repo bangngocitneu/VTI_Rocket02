8a2be52fec4a19e92bec162ae7c9f9e2bb9652b1
bangngoitneu <ngocmapb1@gmail.com>
2019-12-26 17:14:41 +0700

    sua assignment3

--- mySQL/assignment3/Assignment3.sql
+++ mySQL/assignment3/Assignment3.sql
@@ -19,21 +19,21 @@ CREATE TABLE 				Trainee
 		Training_Class		CHAR(10) 	NOT NULL,
 		ET_English			TINYINT 	NOT NULL
 );		
-ALTER TABLE TRAINEE ADD VTI_Account CHAR(10) NOT NULL UNIQUE;
+ALTER TABLE TRAINEE ADD VTI_Account CHAR(20) NOT NULL UNIQUE;
 
 -- Assignment3 add 10 records 
 INSERT INTO Trainee (Full_Name,		 Birth_Date, 	Gender, 	ET_IQ, ET_Gmath, Training_Class, ET_English 	, VTI_Account) 
 VALUES			
-					("Nguyen Thi Ngoc",   '1998-09-01',	"Female",	8,		7,		"RK01",			 20,			"ngocnguyen"),
-					("Nguyen Le Minh",	 '1999-12-16',	"Male",		20,		18,		"RK01",			 40,			"minhle"),
-					("Nguyen Van Anh",	 '1997-11-23',	"Female",	9,		9,		"RK02",			 30,			"vananh"),
-					("Nguyen Khanh Ngoc", '1998-10-24',	"Female",	10,		10,		"RK01",			 35,			"khanhngoc"),
-					("Nguyen Dang Khoa",  '1998-10-25',	"Male",		9,		7,		"RK01",			 25,			"dangkhoa"),
-					("Nguyen Thi Van",    '1999-01-26',	"Female",	8,		8,		"RK01",			 10,			"van"),
-					("Nguyen Thi Thuong", '1998-02-20',	"Female",	25,		20,		"RK02",			 30,			"thuong"),
-					("Nguyen Thuy Linh",  '1997-12-15',	"Female",	9,		10,		"RK01",	 		 15,			"linh"),
-					("Nguyen Quoc Dat",   '1998-12-13',	"Male",		9,		8,		"RK02",			 17,			"dat"),
-					("Nguyen Tuan Vu",    '1999-04-12',	"Male",		6,		8,		"RK01",			 18,		 	"tuanvu");
+					("Nguyen Thi Ngoc",   '1998-09-01',	"Female",	8,		7,		"RK01",			 20,			"ngoc@gmai.com"),
+					("Nguyen Le Minh",	 '1999-12-16',	"Male",		20,		18,		"RK01",			 40,			"minhle@gmai.com"),
+					("Nguyen Van Anh",	 '1997-11-23',	"Female",	9,		9,		"RK02",			 30,			"vananh@gmai.com"),
+					("Nguyen Khanh Ngoc", '1998-10-24',	"Unknown",	10,		10,		"RK01",			 35,			"khanhngoc@gmai.com"),
+					("Nguyen Dang Khoa",  '1998-10-25',	"Male",		9,		7,		"RK01",			 25,			"dangkhoa@gmai.com"),
+					("Nguyen Thi Van",    '1999-01-26',	"Unknown",	8,		8,		"RK01",			 10,			"van@gmai.com"),
+					("Nguyen Thi Thuong", '1998-02-20',	"Female",	25,		20,		"RK02",			 30,			"thuong@gmai.com"),
+					("Nguyen Thuy Linh",  '1997-12-15',	"Female",	9,		10,		"RK01",	 		 15,			"linh@gmai.com"),
+					("Nguyen Quoc Dat",   '1998-12-13',	"Male",		9,		8,		"RK02",			 17,			"dat@gmai.com"),
+					("Nguyen Tuan Vu",    '1999-04-12',	"Male",		6,		8,		"RK01",			 18,		 	"tuanvu@gmai.com");
 -- E2
 -- lay ra tat ca thuc tap sinh vuot qua bai test dau vao nhom theo thang sinh khac nhau
 SELECT 		MONTH(Birth_Date), GROUP_CONCAT(Full_Name)
--- mySQL/assignment5/Assignment5.sql
+++ mySQL/assignment5/Assignment5.sql
@@ -29,4 +29,18 @@ WHERE	ProductSubcategoryID   IN 	(	SELECT 	ProductSubcategoryID
 													WHERE	ProductSubcategoryID   IN 	(	SELECT 	ProductSubcategoryID
 																							FROM	productSubcategory AS PS
 																							WHERE	PS.Name = "Touring Bikes" ));	
--- 
+-- JOIN NHIEU BANG
+-- CAU 1
+SELECT	CT.Name AS Country, SP.Name	AS Provine
+FROM	countryregion AS CT
+JOIN	stateprovince AS SP	ON	CT.CountryRegionCode = SP.CountryRegionCode;
+-- CAU 2
+SELECT	CT.Name AS Country, SP.Name	AS Provine
+FROM	countryregion AS CT
+JOIN	stateprovince AS SP	ON	CT.CountryRegionCode = SP.CountryRegionCode
+WHERE	CT.Name IN ( SELECT CT.Name
+					 FROM	countryregion AS CT
+                     WHERE	Name = "Canada" OR Name = "Germany");
+-- CAU 3
+
+
